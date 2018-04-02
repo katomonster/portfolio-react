5f8858e7b128157ec6bbcdf2243d59a90e9ac00e
katomonster <junekato@gmail.com>
2018-04-02 12:31:45 -0700

    convert files to pure components; use LESS and compile

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -1,38 +1,32 @@
-import React, { Component } from 'react';
-//import Parser from 'html-react-parser';
-class ImageViewer extends Component {
-  render() {
-  	const { viewerLink, viewerTools, viewerUrl, viewerAlt } = this.props;
-    const imageStrips = [];
-    for (let i = 0; i < 10; i++) {
-      imageStrips.push(<div key = {i} className = 'image-strip'></div>);
-    }
-    const launchLink = viewerUrl ? <a className = 'launch-btn' href = {viewerUrl} target='_blank'>Launch</a> : ``;
+import React from 'react';
 
-    return (
-      <figure  className = {viewerLink ? 'active image-viewer' : 'hide image-viewer'}>
-        <div className = 'viewer-overlay' onClick = {() => this.props.onCloseClick()}></div>
-        <div className = 'image-strip-container'>
-          { imageStrips }
-          <div className = 'image-strip'>
-            <img src = {viewerLink} className = 'hide' alt = {viewerAlt || `image here`}/>
-          </div>
-          <i className = 'close-btn' onClick = {() => this.props.onCloseClick()}>&times;</i>
-          <a className = 'prev-btn' onClick = {() => this.props.onPrevClick()}>Prev</a>
-          <a className = 'next-btn' onClick = {() => this.props.onNextClick()}>Prev</a>
+const ImageViewer = (props) => {
+	const { viewerLink, viewerTools, viewerUrl, viewerAlt } = props;
+  const imageStrips = [];
+  for (let i = 0; i < 10; i++) {
+    imageStrips.push(<div key = {i} className = 'image-strip'></div>);
+  }
+  const launchLink = viewerUrl ? <a className = 'launch-btn' href = {viewerUrl} target='_blank'>Launch</a> : ``;
+
+  return (
+    <figure  className = {viewerLink ? 'active image-viewer' : 'hide image-viewer'}>
+      <div className = 'viewer-overlay' onClick = {() => props.onCloseClick()}></div>
+      <div className = 'image-strip-container'>
+        { imageStrips }
+        <div className = 'image-strip'>
+          <img src = {viewerLink} className = 'hide' alt = {viewerAlt || `image here`}/>
         </div>
-        <figcaption>
-          <p>What: {viewerAlt}</p>
-          <p>Tools: <span  dangerouslySetInnerHTML={{__html: viewerTools}} /></p>
-          {launchLink}
-        </figcaption>  
-      </figure>
-    );
-  }
-
-  _unescapeStr(str) {
-    return str? str.replace(/>/g, '&gt;').replace(/</g, '&lt;') : '';
-  }
+        <i className = 'close-btn' onClick = {() => props.onCloseClick()}>&times;</i>
+        <a className = 'prev-btn' onClick = {() => props.onPrevClick()}>Prev</a>
+        <a className = 'next-btn' onClick = {() => props.onNextClick()}>Prev</a>
+      </div>
+      <figcaption>
+        <p>What: {viewerAlt}</p>
+        <p>Tools: <span  dangerouslySetInnerHTML = {{__html: viewerTools}} /></p>
+        {launchLink}
+      </figcaption>  
+    </figure>
+  ); 
 }
 
 export default ImageViewer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   NavLink,
   Route,
@@ -7,71 +7,12 @@ import {
 
 import Pages from './Pages';
 
-class Main extends Component {
-  render() {
-    return (
-      <main>
-        <div id='main-bg'></div>
-        <HashRouter>
-          <div>
-            <header id = 'main-header'>
-              <div id = 'brand-container'>
-                <NavLink exact to = '/'  id = 'brand-logo'>Katomonster logo</NavLink>
-                <div id = 'brand-id'>
-                  <NavLink exact to = '/'>
-                    <span id = 'kato'>Kato</span>
-                    <span id = 'monster'>Monster</span>
-                  </NavLink>
-                  <div id = "brand-tag"></div>
-                </div>
-              </div>
-              <a href = '#to' className = 'hamburger-menu' onClick = {(e) => this._toggleHamburgerMenu(e)} >
-                <i></i>
-                <i></i>
-                <i></i>
-              </a>
-              <nav>
-                <div id="topNavigation">
-                  <header>Development:</header>
-                  <NavLink exact to = '/recent-work'>Recent Work</NavLink>
-                  <NavLink exact to = '/flash'>Flash</NavLink>
-                  <NavLink exact to = '/html5'>HTML5</NavLink>
-                  <NavLink exact to = '/animation'>Animation</NavLink>
-                  <NavLink exact to = '/kids-games'>Kids Games</NavLink>
-                  <NavLink exact to = '/banner'>Rich Media</NavLink>
-                </div>
-                <div id="bottomNavigation">
-                  <header>Design:</header>
-                  <NavLink exact to = '/web-design'>Web Design</NavLink>
-                  <NavLink exact to = '/logo-design'>Logo Design</NavLink>
-                  <NavLink exact to = '/artwork'>Artwork</NavLink>
-                  <NavLink exact to = '/print'>Print</NavLink>
-                </div>
-              </nav>
-            </header>
-
-            <Route exact path = '/' render = {() => this._renderPage('./json/recentWork.json' ,'Home Page')}/>
-            <Route exact path = '/flash' render = {() => this._renderPage('./json/flash.json', 'ActionScript3 pages')} />
-            <Route exact path = '/animation' render = {() => this._renderPage('./json/animation.json', 'Animations • Film Opening • Splash Pages')} />
-            <Route exact path = '/artwork' render = {() => this._renderPage('./json/artwork.json', 'Artwork')} />
-            <Route exact path = '/banner' render = {() => this._renderPage('./json/banner.json', 'Rich Media • Banner Ads')} />
-            <Route exact path = '/html5' render = {() => this._renderPage('./json/html.json', 'HTML5 • CSS • JavaScript • jQuery')} />
-            <Route exact path = '/kids-games' render = {() => this._renderPage('./json/kidsGames.json', 'Kids Games')} />
-            <Route exact path = '/logo-design' render = {() => this._renderPage('./json/logoDesign.json', 'Logo Design')} />
-            <Route exact path = '/print' render = {() => this._renderPage('./json/print.json', 'Print Design')} />
-            <Route exact path = '/recent-work' render = {() => this._renderPage('./json/recentWork.json', 'My Recent Work')} />
-            <Route exact path = '/web-design' render = {() => this._renderPage('./json/webDesign.json', 'Web Site Design')} />
-          </div>
-        </HashRouter>
-      </main>
-    );
-  }
-
-  _renderPage(filename, pagename) {
+const Main = (props) => {
+  const _renderPage = (filename, pagename) => {
     return (<Pages jsonFile = {filename} pageName = {pagename} ></Pages>);
   }
 
-  _toggleHamburgerMenu(e) {
+  const _toggleHamburgerMenu = (e) => {
     e.preventDefault();
     const $body = document.querySelector('body');
     if ($body.className === '') {
@@ -81,6 +22,63 @@ class Main extends Component {
       $body.className = ''
     }
   }
+
+  return (
+    <main>
+      <div id='main-bg'></div>
+      <HashRouter>
+        <div>
+          <header id = 'main-header'>
+            <div id = 'brand-container'>
+              <NavLink exact to = '/'  id = 'brand-logo'>Katomonster logo</NavLink>
+              <div id = 'brand-id'>
+                <NavLink exact to = '/'>
+                  <span id = 'kato'>Kato</span>
+                  <span id = 'monster'>Monster</span>
+                </NavLink>
+                <div id = "brand-tag"></div>
+              </div>
+            </div>
+            <a href = '#to' className = 'hamburger-menu' onClick = {(e) => _toggleHamburgerMenu(e)} >
+              <i></i>
+              <i></i>
+              <i></i>
+            </a>
+            <nav>
+              <div id="topNavigation">
+                <header>Development:</header>
+                <NavLink exact to = '/recent-work'>Recent Work</NavLink>
+                <NavLink exact to = '/flash'>Flash</NavLink>
+                <NavLink exact to = '/html5'>HTML5</NavLink>
+                <NavLink exact to = '/animation'>Animation</NavLink>
+                <NavLink exact to = '/kids-games'>Kids Games</NavLink>
+                <NavLink exact to = '/banner'>Rich Media</NavLink>
+              </div>
+              <div id="bottomNavigation">
+                <header>Design:</header>
+                <NavLink exact to = '/web-design'>Web Design</NavLink>
+                <NavLink exact to = '/logo-design'>Logo Design</NavLink>
+                <NavLink exact to = '/artwork'>Artwork</NavLink>
+                <NavLink exact to = '/print'>Print</NavLink>
+              </div>
+            </nav>
+          </header>
+
+          <Route exact path = '/' render = {() => _renderPage('./json/recentWork.json' ,'Home Page')}/>
+          <Route exact path = '/flash' render = {() => _renderPage('./json/flash.json', 'ActionScript3 pages')} />
+          <Route exact path = '/animation' render = {() => _renderPage('./json/animation.json', 'Animations • Film Opening • Splash Pages')} />
+          <Route exact path = '/artwork' render = {() => _renderPage('./json/artwork.json', 'Artwork')} />
+          <Route exact path = '/banner' render = {() => _renderPage('./json/banner.json', 'Rich Media • Banner Ads')} />
+          <Route exact path = '/html5' render = {() => _renderPage('./json/html.json', 'HTML5 • CSS • JavaScript • jQuery')} />
+          <Route exact path = '/kids-games' render = {() => _renderPage('./json/kidsGames.json', 'Kids Games')} />
+          <Route exact path = '/logo-design' render = {() => _renderPage('./json/logoDesign.json', 'Logo Design')} />
+          <Route exact path = '/print' render = {() => _renderPage('./json/print.json', 'Print Design')} />
+          <Route exact path = '/recent-work' render = {() => _renderPage('./json/recentWork.json', 'My Recent Work')} />
+          <Route exact path = '/web-design' render = {() => _renderPage('./json/webDesign.json', 'Web Site Design')} />
+        </div>
+      </HashRouter>
+    </main>
+  );  
 }
 
 export default Main;
